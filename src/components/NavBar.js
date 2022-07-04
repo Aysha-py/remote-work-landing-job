b116d5811f6e02ee68ccf9feaa49f0d6e8c6b0da
Aysha Muhammed <m.aishat9890@gmail.com>
2022-07-04 09:26:56 -0700

    Add all

---
+++ src/components/NavBar.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import imageherodesktop from "../assets/images/image-hero-desktop.png"
+import databiz from "../assets/images/client-databiz.svg"
+import audiophile from "../assets/images/client-audiophile.svg"
+import meet from "../assets/images/client-meet.svg"
+import maker from "../assets/images/client-maker.svg"
+import arrowdown from "../assets/images/icon-arrow-down.svg"
+
+
+const NavBar = () => {
+  return (
+    <div id="Nav-container">
+        <div className='Nav-header'>
+            <div className='Nav-left'>
+                <div className='Nav-logo'>
+                    <h1>snap</h1>
+                </div>
+                <div className='Nav-tabs'>
+                    <ul className='Nav-tabs-list'>
+                        <li>Features <img src={arrowdown} alt="arrowdown"/ ></li>
+                        <li>Company <img src={arrowdown} alt="arrowdown"/ ></li>
+                        <li>Careers</li>
+                        <li>About</li>  
+                    </ul>
+                </div>
+            </div>
+            <div className='Nav-Right'>
+                <h4>Login</h4>
+                <h4 className='register'>Register</h4> 
+            </div>
+        </div>
+
+        <div className='content-body'>
+            <section className='content-right'>
+                <h5>Make<br/> Remote Work</h5>
+
+                <p>Get your team in sync, No matter the location.
+                    Streamline processes, Create team rituals, 
+                    and watch productivity soar
+                </p>
+                <button>Learn More</button>
+
+                <div className='clients'>
+                   <ul>
+                    <li><img src={databiz} alt="databiz"/></li>
+                    <li><img src={audiophile} alt="audiophile"/></li>
+                    <li><img src={meet} alt="meet"/></li>
+                    <li><img src={maker} alt="maker"/></li>
+                   </ul>
+                
+                </div>
+            </section>
+
+            <section className='hero'>
+                <img src={imageherodesktop} alt='desktopimg'></img>
+            </section>
+
+
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
